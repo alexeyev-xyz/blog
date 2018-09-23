6bb78907fc712c1871c8d65672bb96369d98d1f7
Vladislav Alex <vladislav.j.alexeyev@gmail.com>
2018-09-23 22:54:47 +0200

    Change image urls

--- convert.php
+++ convert.php
@@ -106,9 +106,14 @@ if($allFiles){
 
 		if(isset($imgMatches['filename']))
 			foreach($imgMatches['filename'] as $pImg){
-				$imgToName = "assets/img/".(pathinfo($pImg, PATHINFO_FILENAME).'.'.pathinfo($pImg, PATHINFO_EXTENSION));
-				echo "{$pImg} \n";
-				var_dump(file_put_contents($imgToName, file_get_contents($pImg)));	
+				$imgToName = (pathinfo($pImg, PATHINFO_FILENAME).'.'.pathinfo($pImg, PATHINFO_EXTENSION));
+				$imgToNameFull = "assets/img/".$imgToName;
+				echo "{$pImg} --> $imgToName \n";
+
+				str_replace($pImg, $imgToName, $fileContents);
+				#var_dump(file_put_contents($imgToName, file_get_contents($pImg)));	
+
+
 			}
 
 		$image = '';
